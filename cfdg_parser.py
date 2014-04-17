1592a4160aa2389e4b6734727ad9b80736baa5ca
Jordan Cazamias <jacazamias@gmail.com>
2014-04-17 11:07:12 -0500

    Updated regexes for grammar parsing

--- cfdg_parser.py
+++ cfdg_parser.py
@@ -4,6 +4,9 @@
 
 import re
 
-startshape_regex = r"\s*startshape\s+(\w+)\n"
-shape_regex = r"\s*shape\s+(\w+)"
-single_rule_regex = shape_regex + ""
+startshape_regex = r"\s*startshape\s+(?P<startshapename>\w+)\n"
+shape_regex = r"\s*(?<!start)shape\s+(?P<shapename>\w+)"
+single_rule_regex = shape_regex + r"\s*(?:\n\s*\{|\{)\n?(?:[^\}]*\n?)+\s*\}"
+rule_regex = "\s*rule\s+(?P<ruleweight>[\d\-\.]*\s*)(?:\n\s*\{|\{)\n?(?:[^\}]*\n?)+\s*\}"
+
+
