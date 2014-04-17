f79377de9d5c23c1ff4fad5ad5ae031b536852a6
Jordan <jacazamias@gmail.com>
2014-04-16 21:51:38 -0500

    Added two grammars, cfdg parser file

---
+++ cfdg-parser.py
@@ -0,0 +1,9 @@
+#  Jordan Cazamias
+#  CFDG Optimizer
+#  April 2014
+
+import re
+
+startshape_regex = r"\s*startshape\s+(\w+)\n"
+shape_regex = r"\s*shape\s+(\w+)"
+single_rule_regex = shape_regex + ""
