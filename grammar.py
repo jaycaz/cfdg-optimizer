8183193658b1fdc3a628d28c8a8bf5a0e1f669ae
Jordan <jacazamias@gmail.com>
2014-04-22 17:08:41 -0500

    Updated grammar definitions and representations

--- grammar.py
+++ grammar.py
@@ -4,10 +4,10 @@
 
 
 class Rule:
-	def __init__(self, body = "", weight = 1, isfixed = True):
+	def __init__(self, body = "", weight = 1, fixed = True):
 		self.body = body
 		self.weight = weight
-		self.isfixed = isfixed
+		self.fixed = fixed
 
 class Shape:
 	def __init__(self, name = ""):
@@ -26,9 +26,9 @@ class Grammar:
 	def __repr__(self):
 		string = "Grammar \n{\n"
 		string += "\tShapes:\n"
-		shapenames = [repr(shape) for shape in self.shapes]
-		for name in shapenames: string += "\t\t{0}\n".format(name)
-		string += "\tRules: {0}\n".format(len(self.rules))
+		for shape in self.shapes: string += "\t\t{0} \n\t\t - {1} Rule{2}\n".format(
+				repr(shape), len(shape.rules), "" if len(shape.rules) == 1 else "s")
+		string += "\tTotal Rules: {0}\n".format(len(self.rules))
 		string += "\tStartshape: {0}\n".format(self.startshape)
 		string += "}"
 		return string
